fe518fd5302994fe6c36b0b0eaff710d721b79c8
AzuosShazous <lucasdasghagasdesouza@gmail.com>
2024-12-21 21:07:40 -0300

    Altera nome do arquivo

---
+++ exercicios-introducao_a_programacao_em_C/impressao.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/* Pedir que o usuário digite um inteiro. Terminar a men-
+sagem com um sinal de dois pontos (:) seguido por um
+espaço, e deixar o cursor posicionado após o espaço. */
+
+void main(void)
+{
+    printf("Digite um número: ");
+}
