fd2d2c8e24db5fb0087915ffc35be55a8234a840
AzuosShazous <lucasdasghagasdesouza@gmail.com>
2024-12-23 16:45:01 -0300

    Cria exercício 6

---
+++ exercicios-introducao_a_programacao_em_C/impressao-4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/* Imprimir a mensagem “Este é um programa
+em C.” com cada palavra em uma linha separada. */
+
+void main(void)
+{
+    printf("Este\né\num\nprograma\nem\nC.\n");
+}
+    
