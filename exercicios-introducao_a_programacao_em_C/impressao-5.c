8e5594473e6100913fce83708420468ba5ecc505
AzuosShazous <lucasdasghagasdesouza@gmail.com>
2024-12-23 19:16:56 -0300

    Cria exercício 7

---
+++ exercicios-introducao_a_programacao_em_C/impressao-5.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/* Imprimir a mensagem “Este é um programa
+em C.” com as palavras separadas por tabulações. */
+
+void main(void)
+{
+    printf("Este\té\tum\tprograma\tem\tC.\n");
+}
