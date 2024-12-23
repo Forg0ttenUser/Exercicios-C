94dad24ea87cf0bc22b28c790dae6dd30c0bdcf9
AzuosShazous <lucasdasghagasdesouza@gmail.com>
2024-12-23 16:20:59 -0300

    Cria exercício 5

---
+++ exercicios-introducao_a_programacao_em_C/impressao-3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* Imprimir a mensagem “Este é um programa
+em C.” em duas linhas, de modo que a primeira
+linha termine em C. */
+
+void main(void)
+{
+    printf("Este é um programa em\nC.\n");
+}
+    
