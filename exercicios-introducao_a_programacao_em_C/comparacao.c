e430f336dc709745d21d0c00b4346dd00188d9a0
AzuosShazous <lucasdasghagasdesouza@gmail.com>
2024-12-24 22:00:55 -0300

    Adiciona descrição de arquivo

--- exercicios-introducao_a_programacao_em_C/comparacao.c
+++ exercicios-introducao_a_programacao_em_C/comparacao.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+/* Escreva um programa que
+
+peça ao usuário que digite dois inteiros, obtenha os nú-
+meros e depois imprima o maior número seguido das
+
+palavras ‘é maior’. Se os números forem iguais, impri-
+ma a mensagem “Esses números são iguais”. Use
+
+apenas a forma de seleção única da instrução if que
+você aprendeu neste capítulo.*/
 
 void main(void)
 {
