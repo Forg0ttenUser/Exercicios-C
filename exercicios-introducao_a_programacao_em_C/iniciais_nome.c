ed6a8bc50681d11a55e8f25a85c7ce22f5f72e24
AzuosShazous <lucasdasghagasdesouza@gmail.com>
2024-12-26 17:12:54 -0300

    Adiciona caracteres faltantes

--- exercicios-introducao_a_programacao_em_C/iniciais_nome.c
+++ exercicios-introducao_a_programacao_em_C/iniciais_nome.c
@@ -7,6 +7,6 @@ int main(void)
 {
      printf("\n\n\n\nL L L L L L L L L L\nL\nL\nL\nL\n\n\n\n");
      printf("    C C C C C C\n  C             C\nC                 C\nC                 C\nC                 C\n\n\n\n");
-     printf("    S      S S\n  S       SS   S\nS        SS      S\nS       SS       S\nS      SS        S\n  S   SS        S\n    S S\n\n\n\n\n");
+     printf("    S      S S\n  S       SS   S\nS        SS      S\nS       SS       S\nS      SS        S\n  S   SS       S\n    S S      S\n\n\n\n\n");
 
 }
