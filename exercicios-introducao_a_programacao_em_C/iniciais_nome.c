6d92eb16a414193a500896600205e4a8d85a6224
AzuosShazous <lucasdasghagasdesouza@gmail.com>
2024-12-26 17:03:50 -0300

    Cria exercício 13

---
+++ exercicios-introducao_a_programacao_em_C/iniciais_nome.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* Imprima as suas iniciais em letras em bloco no sentido
+vertical da página. */
+
+int main(void)
+{
+     printf("\n\n\n\nL L L L L L L L L L\nL\nL\nL\nL\n\n\n\n");
+     printf("    C C C C C C\n  C             C\nC                 C\nC                 C\nC                 C\n\n\n\n");
+     printf("    S      S S\n  S       SS   S\nS        SS      S\nS       SS       S\nS      SS        S\n  S   SS        S\n    S S\n\n\n\n\n");
+
+}
